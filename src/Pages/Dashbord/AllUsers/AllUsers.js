c7e629237703e28d6d908528809d56bbca5ed991
mdaslamalidu <thehometutor2022@gmail.com>
2022-11-18 11:42:15 +0600

    add user admin

---
+++ src/Pages/Dashbord/AllUsers/AllUsers.js
@@ -0,0 +1,64 @@
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+
+const AllUsers = () => {
+  const { data: users = [] } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await fetch("http://localhost:5000/users");
+      const data = await res.json();
+      return data;
+    },
+  });
+
+  const handleAdmin = (id) => {
+    fetch(`http://localhost:5000/users/admin/${id}`, {
+      method: "PUT",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
+  return (
+    <div>
+      <h2 className="text-3xl">All Users</h2>
+      <div className="overflow-x-auto">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Favorite Color</th>
+              <th>Favorite Color</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr key={user._id}>
+                <th>{index + 1}</th>
+                <td>{user.name}</td>
+                <td>{user.email}</td>
+                <td>
+                  {user?.role !== "admin" && (
+                    <button
+                      onClick={() => handleAdmin(user?._id)}
+                      className="btn btn-primary btn-xs"
+                    >
+                      Make Admin
+                    </button>
+                  )}
+                </td>
+                <td>Blue</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default AllUsers;
--- src/Router/routes.js
+++ src/Router/routes.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import DahsboardLayout from "../Layout/DahsboardLayout";
 import Main from "../Layout/Main";
 import Appoinments from "../Pages/Appoinments/Appoinments/Appoinments";
+import AllUsers from "../Pages/Dashbord/AllUsers/AllUsers";
 import MyAppointment from "../Pages/Dashbord/MyAppointment/MyAppointment";
 import Home from "../Pages/Home/Home/Home";
 import Login from "../Pages/Login/Login";
@@ -42,6 +43,10 @@ export const routes = createBrowserRouter([
       {
         path: "/dashbord",
         element: <MyAppointment></MyAppointment>,
+      },
+      {
+        path: "/dashbord/allUsers",
+        element: <AllUsers></AllUsers>,
       },
     ],
   },
