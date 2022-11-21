a0635a822dfda6dcff2630a2ec43afdca4873653
mdaslamalidu <thehometutor2022@gmail.com>
2022-11-21 10:31:50 +0600

    stype payment section

--- src/Pages/Dashbord/Payment/Payment.js
+++ src/Pages/Dashbord/Payment/Payment.js
@@ -3,10 +3,14 @@ import { useLoaderData } from "react-router-dom";
 
 const Payment = () => {
   const bookings = useLoaderData();
-  console.log(bookings);
+  const { appointmentDate, treatmentName, slot, price } = bookings;
   return (
     <div>
-      <h3 className="text-3xl">Payment</h3>
+      <h3 className="text-3xl">Payment for {treatmentName} </h3>
+      <p className="text-2xl">
+        please pay <strong>${price}</strong> for your appointment on{" "}
+        {appointmentDate} at {slot}
+      </p>
     </div>
   );
 };
